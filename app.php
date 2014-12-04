87195a55343e1937a10cbd4f9aa8faa1209825a9
Martins Sipenko <martins.sipenko@accenture.com>
2014-12-04 11:37:48 +0000

    Demo app

---
+++ app.php
@@ -0,0 +1,22 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+use Silex\Application;
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+$app = new Silex\Application();
+
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('php://stdout', Logger::WARNING));
+
+
+$app->get('/hello/{name}', function ($name) use ($app, $log) {
+    $log->addWarning('Foo');
+    $log->addError('Bar');
+    return "\n\nHello ".$app->escape($name);
+});
+
+$app->run();
